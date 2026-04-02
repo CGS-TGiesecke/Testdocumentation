5147c0cb2d69a7ea684140d12297caaa0abb8fab
Thomas Giesecke <TGiesecke@cgs-online.de>
2026-04-02 08:03:42 +0200

    test

--- onboardingCatalog/de/table-style.lua
+++ onboardingCatalog/de/table-style.lua
@@ -7,26 +7,29 @@
 -- funktioniert auch mit [.TableGrid] im Asciidoc
 -- funktioniert unabhängig davon, wie Asciidoctor Tabellen rendert
 
+-- Apply style to HTML tables that Pandoc does NOT parse as Table objects
+function RawBlock(el)
+  if el.format == "html" then
+    -- Jede Art von <table> wird ersetzt, egal wie Asciidoctor sie erzeugt
+    if el.text:match("<table") then
+      el.text = el.text:gsub("<table", '<table custom-style="Table Grid"')
+    end
+  end
+  return el
+end
+
+function RawInline(el)
+  if el.format == "html" then
+    if el.text:match("<table") then
+      el.text = el.text:gsub("<table", '<table custom-style="Table Grid"')
+    end
+  end
+  return el
+end
 
 -- Apply style to real Pandoc Table elements
 function Table(el)
   el.attributes['custom-style'] = 'Table Grid'
-  return el
-end
-
--- Apply style to HTML tables that Pandoc does NOT parse as Table objects
-function RawBlock(el)
-  if el.format == "html" and el.text:match("<table") then
-    el.text = el.text:gsub("<table", '<table custom-style="Table Grid"')
-  end
-  return el
-end
-
-
-function RawInline(el)
-  if el.format == "html" and el.text:match("<table") then
-    el.text = el.text:gsub("<table", '<table custom-style="Table Grid"')
-  end
   return el
 end
 
