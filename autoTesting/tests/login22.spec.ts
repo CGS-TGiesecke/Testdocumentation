ab9ebc8800d1d0cbb15dbe1d0d096b1f74f0414f
Thomas Giesecke <TGiesecke@cgs-online.de>
2026-04-20 15:25:15 +0200

    test

--- autoTesting/tests/login22.spec.ts
+++ autoTesting/tests/login22.spec.ts
@@ -2,9 +2,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Login wird angezeigt', async ({ page }) => {
+
   await page.goto('/');
 
   await expect(
     page.getByText('Portal')
   ).toBeVisible();
+  
 });
