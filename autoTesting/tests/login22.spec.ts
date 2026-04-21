7a1e835cb01c649f582d833148360009a41c8e8a
Thomas Giesecke <TGiesecke@cgs-online.de>
2026-04-21 08:04:57 +0200

    test

--- autoTesting/tests/login22.spec.ts
+++ autoTesting/tests/login22.spec.ts
@@ -5,11 +5,17 @@ test('Startseite wird angezeigt', async ({ page }) => {
 
   await page.goto('/');
 
-  await expect( page.getByText('Portal')).toBeVisible();
+  await expect( page.getByText('Confirmation required')).toBeVisible();
+
+  await expect('button', { name: 'OK' }).toBeVisible();
+
+  await page.getByRole('button', { name: 'OK' }).click();
+
+  await expect('link', { name: 'Automation' }).toBeVisible();
 
   await page.getByRole('link', { name: 'Automation' }).click();
 
-  await page.goto('/automation');
+  await page.waitForURL('**/automation');
 
   await expect(page.getByText('Automated Use Cases')).toBeVisible();
 
